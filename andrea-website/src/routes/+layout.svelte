92c3a5a51f05bad152659a8fd32519628748ce14
BenC <bcuttance1998@gmail.com>
2025-04-05 20:58:10 +1030

    files added

---
+++ andrea-website/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import TopNavBar from '$lib/components/topNavBar.svelte';
+
+	let navLinks = {
+		links: ['Home', 'About', 'Other']
+	};
+</script>
+
+<section>
+	<TopNavBar title={'Kinesiology with Andrea'} {navLinks} />
+</section>
+
+<style>
+</style>
